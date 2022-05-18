508e05340163819d0c163322ccedb045cd1e5bee
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2022-05-18 12:56:18 -0300

    setup rootViewController

--- TwitterIOS/TwitterIOS/Controller/MainTabBarController.swift
+++ TwitterIOS/TwitterIOS/Controller/MainTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  TwitterIOS
 //
 //  Created by Paulo Rodrigues on 18/05/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .blue
     }
 
-
 }
-
