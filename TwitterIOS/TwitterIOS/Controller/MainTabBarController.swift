e81c06c136e297b1b96b2dd4de6e2ae64941d042
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2022-05-18 15:19:45 -0300

    setup navigation controllers

--- TwitterIOS/TwitterIOS/Controller/MainTabBarController.swift
+++ TwitterIOS/TwitterIOS/Controller/MainTabBarController.swift
@@ -17,23 +17,47 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         setupViewControllers()
-        tabBar.backgroundColor = .systemPink
+        tabBar.backgroundColor = .systemGray
     }
     
     // MARK: - Helpers
     
     func setupViewControllers() {
         let feedViewController = FeedViewController()
+        
+        let navigationController = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feedViewController)
+        
         let exploreViewController = ExploreViewController()
+        
+        let navigationController2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: exploreViewController)
+        
         let notificationsViewController = NotificationsViewController()
+        
+        let navigationController3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notificationsViewController)
+        
         let conversationsViewController = ConversationsViewController()
         
+        let navigationController4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversationsViewController)
+        
         viewControllers = [
-            feedViewController,
-            exploreViewController,
-            notificationsViewController,
-            conversationsViewController
+            navigationController,
+            navigationController2,
+            navigationController3,
+            navigationController4
         ]
+    }
+    
+    func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.image = image
+        
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .systemGray
+        
+        navigationController.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        
+        return navigationController
     }
 
 }
